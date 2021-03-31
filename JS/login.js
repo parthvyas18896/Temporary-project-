3a366615aa9c746cf524dc5ca4f3f230e8912ced
parthvyas18896 <pvyas1@confederationcollege.ca>
2021-03-31 01:56:01 -0400

    nothing updated

--- JS/login.js
+++ JS/login.js
@@ -27,6 +27,7 @@ $(function () {
 
 $("#createAccount").submit(function (e) {
   var errorMessage = "";
+  var result1 = true;
   e.preventDefault();
 
   function checkuserName(username) {
@@ -39,6 +40,7 @@ $("#createAccount").submit(function (e) {
     errorMessage += "Please enter your name";
 
     $("#signupUsername").css("border-bottom", "2px solid #FF0000");
+    result1 = false;
   } else {
     $("#signupUsername").css("border-bottom", "2px solid #00FF00");
   }
@@ -55,60 +57,51 @@ $("#createAccount").submit(function (e) {
     $("#error").css({ opacity: 0.5 });
     $("#email").css("border-bottom", "2px solid #FF0000");
     errorMessage += "<br> Please enter a valid email address";
+    result1 = false
   } else {
     $("#email").css("border-bottom", "2px solid #00FF00");
   }
 
   
-  function checkpassword(password)
-  {
-    var passwordrequirement = new RegExp(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/);
-    return passwordrequirement.test(password);
-  }
-  if(!checkpassword($("password").val())){
-   $("#error").show();
-   $("#error").css({ opacity: 0.5 });
-   $("#password").css("border-bottom", "2px solid #FF0000");
-   errorMessage += "<br> Please enter at least 6 character in your password \n<br> password must have atleast one symbol <br> password must have at least one upper and lower word ";
-   result = false;
-  }
-  else{
-   $("#password").css("border-bottom", "2px solid #00FF00");
-  }
-
-  // if (
-  //   $("#password").val() != $("#password2").val() ||
-  //   ($("#password").val().length < 8) & ($("#password2").val().length < 8) ||
-  //   $("#password").val() == "" ||
-  //   $("#password2").val() == "" )
-  //   {
-  //   $("#error").show();
-  //   $("#error").css({ opacity: 0.5 });
-  //   $("#password").css("border-bottom", "2px solid #FF0000");
-  //   $("#password2").css("border-bottom", "2px solid #FF0000");
-  //   errorMessage += "<br> Space would be not allow in your password";
-  // } 
-  // if ($("password").val().seach(/[a-z]/i) < 0)
-  // {
-  //   $("#error").show();
-  //   $("#error").css({ opacity: 0.5 });
-  //   $("#password").css("border-bottom", "2px solid #FF0000");
-  //   $("#password2").css("border-bottom", "2px solid #FF0000");
-  //   errorMessage += "<br> your password must have at least one letter,<br>";
-  // }
-  // if ($("password").val().seach(/[0-9]/)< 0)
-  // {
-  //   $("#error").show();
-  //   $("#error").css({ opacity: 0.5 });
-  //   $("#password").css("border-bottom", "2px solid #FF0000");
-  //   $("#password2").css("border-bottom", "2px solid #FF0000");
-  //   errorMessage +="<br> your password must have at least one digit,<br>";
-  // }
+//   function checkpassword (str){
+//     var passwordrequirement = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+//     return passwordrequirement.test(str);
+// }
   
-  // else {
-  //   $("#password").css("border-bottom", "2px solid #00FF00");
-  //   $("#password2").css("border-bottom", "2px solid #00FF00");
-  // }
+//   if(!checkpassword($("password").val()))
+//   {
+//    $("#error").show();
+//    $("#error").css({ opacity: 0.5 });
+//    $("#password").css("border-bottom", "2px solid #FF0000");
+//    $("#password2").css("border-bottom", "2px solid #FF0000");
+//    errorMessage += "<br> Please enter at least 6 character in your password \n<br> password must have atleast one symbol <br> password must have at least one upper and lower word ";
+//    result = false;
+//   }
+//   else
+//   {
+//    $("#password").css("border-bottom", "2px solid #00FF00");
+//    $("#password2").css("border-bottom", "2px solid #00FF00");
+//   }
+
+  if (
+    $("#password").val() != $("#password2").val() ||
+    ($("#password").val().length < 8) || ($("#password2").val().length < 8) ||
+    $("#password").val() == "" ||
+    $("#password2").val() == "" ||
+    $("password").val() !== $("#signupUsername").val() )
+    {
+    $("#error").show();
+    $("#error").css({ opacity: 0.5 });
+    $("#password").css("border-bottom", "2px solid #FF0000");
+    $("#password2").css("border-bottom", "2px solid #FF0000");
+    errorMessage += "<br> Password must be match with confirm password <br> Please make sure password does not similar to username <br> Password must be more than 8 characters";
+    
+  } 
+  
+  else {
+    $("#password").css("border-bottom", "2px solid #00FF00");
+    $("#password2").css("border-bottom", "2px solid #00FF00");
+  }
 
   if (errorMessage == "") {
     $("#error").hide();
@@ -118,6 +111,7 @@ $("#createAccount").submit(function (e) {
     });
   } else {
     $("#error").html(errorMessage);
+    result1=false;
   }
 });
 
@@ -159,39 +153,38 @@ $("#resetpassword").submit(function (e) {
   }
 
 
-   function checkpassword(password)
-   {
-     var passwordrequirement = new RegExp(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/);
-     return passwordrequirement.test(password);
-   }
-   if(!checkpassword($("rpassword").val())){
-    $("#error").show();
-    $("#error").css({ opacity: 0.5 });
-    $("#rpassword").css("border-bottom", "2px solid #FF0000");
-    errorMessage += "<br> Please enter a valid email address";
-    result = false;
-   }
-   else{
-    $("#rpassword").css("border-bottom", "2px solid #00FF00");
-   }
-  // if (
-  //   $("#rpassword").val() != $("#rpassword2").val() ||
-  //   ($("#rpassword").val().length < 8) & ($("#rpassword2").val().length < 8) ||
-  //   $("#rpassword").val() == "" ||
-  //   $("#rpassword2").val() == "" ||
-  //   $("#rpassword").val().includes("@") !== true
-  // ) {
+  //  function checkpassword(password)
+  //  {
+  //    var passwordrequirement = new RegExp(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/);
+  //    return passwordrequirement.test(password);
+  //  }
+  //  if(!checkpassword($("rpassword").val())){
   //   $("#error").show();
   //   $("#error").css({ opacity: 0.5 });
   //   $("#rpassword").css("border-bottom", "2px solid #FF0000");
-  //   $("#rpassword2").css("border-bottom", "2px solid #FF0000");
-  //   errorMessage +=
-  //     "<br> Password  must match with confirm password,<br> Password must have more than 8 character <br> Password must have @ somewhere";
+  //   errorMessage += "<br> Please enter a valid email address";
   //   result = false;
-  // } else {
+  //  }
+  //  else{
   //   $("#rpassword").css("border-bottom", "2px solid #00FF00");
-  //   $("#rpassword2").css("border-bottom", "2px solid #00FF00");
-  // }
+  //  }
+  if (
+    $("#rpassword").val() != $("#rpassword2").val() ||
+    ($("#rpassword").val().length < 8) & ($("#rpassword2").val().length < 8) ||
+    $("#rpassword").val() == "" ||
+    $("#rpassword2").val() == "" 
+  ) {
+    $("#error").show();
+    $("#error").css({ opacity: 0.5 });
+    $("#rpassword").css("border-bottom", "2px solid #FF0000");
+    $("#rpassword2").css("border-bottom", "2px solid #FF0000");
+    errorMessage +=
+      "<br> Password  must match with confirm password,<br> Password must have more than 8 character <br>";
+    result = false;
+  } else {
+    $("#rpassword").css("border-bottom", "2px solid #00FF00");
+    $("#rpassword2").css("border-bottom", "2px solid #00FF00");
+  }
 
   if (errorMessage == "") {
     $("#error").hide();
